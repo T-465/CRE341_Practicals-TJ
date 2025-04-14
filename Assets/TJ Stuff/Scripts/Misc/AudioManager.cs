c1d8dc0bcef9f89b5b5613be8967c3779bf09b08
T-465 <Dunne-T2@ulster.ac.uk>
2025-04-14 19:07:24 +0100

    audio manager

---
+++ Assets/TJ Stuff/Scripts/Misc/AudioManager.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AudioManager : MonoBehaviour
+{
+    public Singleton singleton;
+    public static AudioManager instance; 
+    public Sound[] musicSounds, sfxSounds;
+    public AudioSource musicSource;
+    public AudioSource sfxSource;
+
+
+    private void Awake()
+    {
+        singleton = GameObject.FindWithTag("singleton").GetComponent<Singleton>();
+        if (instance == null)
+        {
+            DontDestroyOnLoad(this.gameObject);
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+        private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        Start();
+    }
+
+    public void Start()
+    {
+        singleton = GameObject.FindWithTag("singleton").GetComponent<Singleton>();
+        
+        if (SceneManager.GetActiveScene().name == "Main Menu")
+        {
+            PlayMusic("Main Menu Theme");
+        }
+        else if (SceneManager.GetActiveScene().name == "GhostLight")
+        {
+            PlayMusic("Game Theme");
+        }
+   
+        
+
+    }
+    public void PlayMusic (string name)
+    {
+        Sound s = System.Array.Find(musicSounds, sound => sound.name == name);
+        if (s == null) return;
+        musicSource.clip = s.clip;
+        musicSource.Play();
+    }
+    public void PlaySFX (string name)
+    {
+        Sound s = System.Array.Find(sfxSounds, sound => sound.name == name);
+        if (s == null) return;
+        sfxSource.clip = s.clip;
+        sfxSource.Play();
+    }
+    private void Update()
+    {
+        
+
+    }
+}
